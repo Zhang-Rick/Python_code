f830f085b28e78f259f9a4a2a093882918be90f3
ee364b21 <zhan2642@purdue.edu>
2019-04-14 19:13:57 -0400

    bash 2:51

--- Prelab12/Morphing.py
+++ Prelab12/Morphing.py
@@ -152,15 +152,5 @@ class Morpher():
             image[int(points[i][1])][int(points[i][0])] = self.leftImage[int(points[i][1])][int(points[i][0])] * (1-alpha) + self.rightImage[int(points[i][1])][int(points[i][0])] * (alpha)
             i += 1
         return image
-def bilinear_interpolation(x, y, points):
 
-    points = sorted(points)               # order points by x, then by y
-    (x1, y1, q11), (_x1, y2, q12), (x2, _y1, q21), (_x2, _y2, q22) = points
-
-    if x1 != _x1 or x2 != _x2 or y1 != _y1 or y2 != _y2:
-        raise ValueError('points do not form a rectangle')
-    if not x1 <= x <= x2 or not y1 <= y <= y2:
-        raise ValueError('(x, y) not within the rectangle')
-
-    return (q11 * (x2 - x) * (y2 - y) +q21 * (x - x1) * (y2 - y) +q12 * (x2 - x) * (y - y1) +q22 * (x - x1) * (y - y1)) / ((x2 - x1) * (y2 - y1) + 0.0)
 
